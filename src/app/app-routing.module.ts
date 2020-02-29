a40e58fd6973d3540be370e81b2af0f99bbbd003
AwrenNuit <a.rennaker@yahoo.com>
2020-02-29 11:49:37 -0600

    set up routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: '/home', component: HomeComponent },
+  { path: '/about', component: AboutComponent },
+  { path: '/contact', component: ContactComponent },
+  { path: '/products', component: ProductsComponent },
+  { path: '/product-detail/:id', component: ProductDetailComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
