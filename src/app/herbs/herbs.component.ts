121038f81d012868faa7535f1abf49b8e7974a1a
AwrenNuit <a.rennaker@yahoo.com>
2020-02-29 12:25:32 -0600

    set up a few herbs in an array

--- src/app/herbs/herbs.component.ts
+++ src/app/herbs/herbs.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HerbsComponent implements OnInit {
 
+  Herbs = [
+    {
+      id: 1,
+      name: 'Elderberry',
+      description: 'Elderberry is great for a cold or the flu.'
+    },
+    {
+      id: 2,
+      name: 'Goldenrod',
+      description: 'Goldenrod is an immune-booster that works wonders for people with allergies.'
+    },
+    {
+      id: 3,
+      name: 'Yarrow',
+      description: 'Yarrow helps cuts stop bleeding.'
+    }
+  ];
+
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
 }
