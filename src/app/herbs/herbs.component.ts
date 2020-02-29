074a085e219d1d15d080a6de94bfba2c71e6c50f
AwrenNuit <a.rennaker@yahoo.com>
2020-02-29 12:41:39 -0600

    styled component

--- src/app/herbs/herbs.component.ts
+++ src/app/herbs/herbs.component.ts
@@ -10,16 +10,36 @@ export class HerbsComponent implements OnInit {
   Herbs = [
     {
       id: 1,
+      name: 'Clove',
+      description: 'Clove has been used to relieve toothaches.'
+    },
+    {
+      id: 2,
+      name: 'Echinacea',
+      description: 'Echinacea is an incredible immune-booster, and even said to fight cancer.'
+    },
+    {
+      id: 3,
       name: 'Elderberry',
       description: 'Elderberry is great for a cold or the flu.'
     },
     {
-      id: 2,
+      id: 4,
       name: 'Goldenrod',
       description: 'Goldenrod is an immune-booster that works wonders for people with allergies.'
     },
     {
-      id: 3,
+      id: 5,
+      name: 'Horehound',
+      description: 'Hoarhound is often used in cough drops.'
+    },
+    {
+      id: 6,
+      name: 'Peppermint',
+      description: 'Peppermint soothes the stomach and aids in digestion.'
+    },
+    {
+      id: 7,
       name: 'Yarrow',
       description: 'Yarrow helps cuts stop bleeding.'
     }
